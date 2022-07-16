34929dd2b0247844df529a8c35cc0b1f2a7d0760
maximiranda <maximiranda.95@gmail.com>
2022-07-16 18:27:42 -0300

    delete extra lines in homebankingAplication

--- src/main/java/com/mindhub/homebanking/HomebankingApplication.java
+++ src/main/java/com/mindhub/homebanking/HomebankingApplication.java
@@ -1,8 +1,7 @@
 package com.mindhub.homebanking;
 
-import com.mindhub.homebanking.models.Account;
+
 import com.mindhub.homebanking.models.Client;
-import com.mindhub.homebanking.repositories.AccountRepository;
 import com.mindhub.homebanking.repositories.ClientRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -18,17 +17,10 @@ public class HomebankingApplication {
 		SpringApplication.run(HomebankingApplication.class, args);
 	}
 	@Bean
-	public CommandLineRunner initData(ClientRepository clientRepository, AccountRepository accountRepository){
+	public CommandLineRunner initData(ClientRepository clientRepository){
 		return (args) -> {
-			Client client1 = new Client("Pedro", "Martinez", "pedromartinez@gmail.com");
-			clientRepository.save(client1);
 			clientRepository.save(new Client("Maxi", "Miranda", "maximiranda.95@gmail.com"));
-			clientRepository.save(new Client("Juan", "Topo", "juantopo@gmail.com"));
-			Account account1 = new Account("VIN001", LocalDateTime.now(), 5000.0);
-			client1.addAccount(account1);
-			accountRepository.save(account1);
 
 		};
 	}
 }
-/*.addAccount(new Account("VIN001", LocalDateTime.now(), 5000.0))*/
