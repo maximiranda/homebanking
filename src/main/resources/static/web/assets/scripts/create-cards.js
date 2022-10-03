50a8395f89885b381b98c0c2a922a449e8f95c3b
maximiranda <maximiranda.95@gmail.com>
2022-10-02 23:47:25 -0300

    card colors it's working

--- src/main/resources/static/web/assets/scripts/create-cards.js
+++ src/main/resources/static/web/assets/scripts/create-cards.js
@@ -9,7 +9,7 @@ createApp({
         creditCards: [],
         debitCards: [],
         hide: true,
-        cardColor: "",
+        cardColor: "TITANIUM",
         cardType: "",
         transactions: [],
         success: false,
